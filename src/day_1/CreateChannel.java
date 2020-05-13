49931b7c07157cfcbb417f8e98da32c150d22abf
dreyski <andriikesler@gmail.com>
2020-05-13 10:58:45 -0400

    update CreateChannel

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,5 +4,11 @@ public class CreateChannel {
 
     public static void main(String[] args) {
         System.out.println("User is able to create a channel");
+
+        if(true) {
+            System.out.println("TRUE");
+        }else {
+            System.out.println("FALSE");
+        }
     }
 }
